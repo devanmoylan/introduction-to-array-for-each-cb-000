e51aef2a1be55bb7ef5f69ba75a80349636cfb6a
Devan <devan.moylan@backyardlion.com>
2017-08-17 12:49:27 -0600

    Done.

--- index.js
+++ index.js
@@ -3,4 +3,9 @@
 function doToElementsInArray(array, callback) {
   array.forEach(callback);
 }
+
 // Add your changeCompletely() function here:
+
+function changeCompletely(element, index, array) {
+  array[index] = + ` ${array[index]}s!!!`;
+}
